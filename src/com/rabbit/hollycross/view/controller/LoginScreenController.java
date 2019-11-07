a72a01aeb90010b248b9be7fa03cebdefbb8ec4a
Shehanka <hcsperera@outlook.com>
2019-11-07 14:07:18 +0530

    Back again to javafx

---
+++ src/com/rabbit/hollycross/view/controller/LoginScreenController.java
@@ -0,0 +1,42 @@
+package com.rabbit.hollycross.view.controller;
+
+import com.jfoenix.controls.JFXPasswordField;
+import com.jfoenix.controls.JFXTextField;
+import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.fxml.Initializable;
+import javafx.scene.layout.AnchorPane;
+
+import java.io.IOException;
+import java.net.URL;
+import java.util.ResourceBundle;
+
+/**
+ * @author chamodshehanka on 11/3/2019
+ * @project StudentManagementSystem
+ **/
+public class LoginScreenController implements Initializable {
+
+    @FXML
+    private JFXTextField txtUserName;
+
+    @FXML
+    private JFXPasswordField txtPassword;
+
+    @Override
+    public void initialize(URL location, ResourceBundle resources) {
+
+    }
+
+    @FXML
+    private void loginAction() {
+        try {
+            AnchorPane principalDashboardPane = FXMLLoader.load(getClass().getResource(
+                    "/com/rabbit/hollycross/view/fxml/PrincipalDashboard.fxml"
+            ));
+
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+}
--- src/index.js
+++
@@ -1,8 +0,0 @@
-import ReactDOM from 'react-dom';
-import './index.css';
-import ROUTING from './routes/routes';
-import * as serviceWorker from './serviceWorker';
-
-ReactDOM.render(ROUTING, document.getElementById('root'));
-
-serviceWorker.unregister();
