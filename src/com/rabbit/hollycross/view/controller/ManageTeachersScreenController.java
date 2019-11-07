9c42e64169bc375f6b30e00c77b403c98cb841ca
Shehanka <hcsperera@outlook.com>
2019-11-07 17:27:21 +0530

    manage teachers

---
+++ src/com/rabbit/hollycross/view/controller/ManageTeachersScreenController.java
@@ -0,0 +1,18 @@
+package com.rabbit.hollycross.view.controller;
+
+import javafx.fxml.Initializable;
+
+import java.net.URL;
+import java.util.ResourceBundle;
+
+/**
+ * @author chamodshehanka on 11/7/2019
+ * @project StudentManagementSystem
+ **/
+public class ManageTeachersScreenController implements Initializable {
+
+    @Override
+    public void initialize(URL location, ResourceBundle resources) {
+
+    }
+}
