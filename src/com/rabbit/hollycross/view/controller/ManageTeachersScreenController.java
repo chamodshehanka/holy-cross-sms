dbba2895a36c3cae8064dba9bcd1f3dd319f70fd
Shehanka <hcsperera@outlook.com>
2019-11-09 02:49:24 +0530

    manage teacher ui done

--- src/com/rabbit/hollycross/view/controller/ManageTeachersScreenController.java
+++ src/com/rabbit/hollycross/view/controller/ManageTeachersScreenController.java
@@ -1,5 +1,7 @@
 package com.rabbit.hollycross.view.controller;
 
+import com.jfoenix.controls.JFXDatePicker;
+import com.jfoenix.controls.JFXTextField;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
 import javafx.scene.layout.AnchorPane;
@@ -16,8 +18,73 @@ public class ManageTeachersScreenController implements Initializable {
     @FXML
     private AnchorPane rootManageTeachers;
 
+    @FXML
+    private JFXTextField txtTeacherID;
+
+    @FXML
+    private JFXTextField txtTeacherName;
+
+    @FXML
+    private JFXTextField txtTeacherNIC;
+
+    @FXML
+    private JFXDatePicker txtAppointedDate;
+
+    @FXML
+    private JFXTextField txtTeacherQualification;
+
+    @FXML
+    private JFXTextField txtTeacherExeperience;
+
+    @FXML
+    private JFXTextField txtTeacherSectionID;
+
+    @FXML
+    private JFXTextField txtTeacherClassRoomID;
+
+    @FXML
+    private JFXTextField txtTeacherECAID;
+
+    @FXML
+    private JFXTextField txtTeacherIDCCAID;
+
+    @FXML
+    private JFXTextField txtTeacherAttendanceID;
+
+    @FXML
+    private JFXTextField txtTeacherReliefID;
+
+    @FXML
+    private JFXTextField txtTeacherHallID;
+
     @Override
     public void initialize(URL location, ResourceBundle resources) {
+
+    }
+
+
+    @FXML
+    void addTeacher() {
+
+    }
+
+    @FXML
+    void allocateRelieves() {
+
+    }
+
+    @FXML
+    void deleteTeacher() {
+
+    }
+
+    @FXML
+    void searchTeacher() {
+
+    }
+
+    @FXML
+    void updateTeacher() {
 
     }
 }
