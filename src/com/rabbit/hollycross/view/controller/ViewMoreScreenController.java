3c963ce888a6df2fdd1fc9f22be44d0a6ac07014
Shehanka <hcsperera@outlook.com>
2019-11-22 17:02:38 +0530

    Manage Grades UI making

--- src/com/rabbit/hollycross/view/controller/ViewMoreScreenController.java
+++ src/com/rabbit/hollycross/view/controller/ViewMoreScreenController.java
@@ -1,7 +1,11 @@
 package com.rabbit.hollycross.view.controller;
 
+import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
 import javafx.fxml.Initializable;
+import javafx.scene.layout.AnchorPane;
 
+import java.io.IOException;
 import java.net.URL;
 import java.util.ResourceBundle;
 
@@ -10,8 +14,68 @@ import java.util.ResourceBundle;
  * @project StudentManagementSystem
  **/
 public class ViewMoreScreenController implements Initializable {
+
+
+    @FXML
+    private AnchorPane rootViewMore;
+
     @Override
     public void initialize(URL location, ResourceBundle resources) {
 
     }
+
+    @FXML
+    private void viewManageSubjects(){
+        try {
+            AnchorPane pane = FXMLLoader
+                    .load(getClass().getResource("/com/rabbit/hollycross/view/fxml/ManageSubjectsScreen.fxml"));
+            rootViewMore.getChildren().setAll(pane);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+
+    @FXML
+    void viewAchievement() {
+
+    }
+
+    @FXML
+    void viewActivities() {
+
+    }
+
+    @FXML
+    void viewClasses() {
+
+    }
+
+    @FXML
+    void viewGrades() {
+        AnchorPane pane = null;
+        try {
+            pane = FXMLLoader
+                    .load(getClass().getResource("/com/rabbit/hollycross/view/fxml/ManageGradesScreen.fxml"));
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        rootViewMore.getChildren().setAll(pane);
+    }
+
+    @FXML
+    void viewManageGrades() {
+
+    }
+
+    @FXML
+    void viewSections() {
+
+    }
+
+    @FXML
+    void viewSubjects() {
+
+    }
+
 }
